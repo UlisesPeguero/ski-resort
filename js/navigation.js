92e778e550569ec9a8ffe0c868d0b2b5e0394bea
UlisesPeguero <peguero.ulises@gmail.com>
2020-11-16 16:50:40 -0600

    Added automated navigation.

---
+++ js/navigation.js
@@ -0,0 +1,86 @@
+var navigation = {
+    'Home': '/',
+    'About': '/about.html',
+    'Amenities': '/amenities.html',
+    'Safety': '/safety.html',
+    'Contact': '/contact.html'
+};
+
+$(function() {
+    var navbar = $('.navbar');
+    navbar.addClass('fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top');
+    navbar.empty();
+    navbar.append('<div class="container">' + 
+                    '<a class="navbar-brand" href="index.html"><img src="img/logo.jpg" alt="Ski Resort\'s Logo"></a>' +
+                        '<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">' + 
+                            '<span class="navbar-toggler-icon"></span>' + 
+                        '</button>' + 
+                        '<div class="collapse navbar-collapse" id="navbarResponsive">' +
+                        '<ul class="navbar-nav ml-auto">');
+    navbar = $('#navbarResponsive > ul.navbar-nav');
+    // loop thru navigation object
+    for(var name in navigation) {
+        var li = $('<li>');
+        li.addClass('nav-item');
+        li.append('<a class="nav-link" href="' + navigation[name] + '">' + name + '</a>');
+        navbar.append(li);
+    }
+});
+
+/*
+
+<div class="container">
+      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services.html">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Portfolio
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+              <a class="dropdown-item" href="portfolio-1-col.html">1 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-2-col.html">2 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-3-col.html">3 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Blog
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+              <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
+              <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
+              <a class="dropdown-item" href="blog-post.html">Blog Post</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPages" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Other Pages
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPages">
+              <a class="dropdown-item" href="full-width.html">Full Width Page</a>
+              <a class="dropdown-item" href="sidebar.html">Sidebar Page</a>
+              <a class="dropdown-item" href="faq.html">FAQ</a>
+              <a class="dropdown-item" href="404.html">404</a>
+              <a class="dropdown-item" href="pricing.html">Pricing Table</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    */
